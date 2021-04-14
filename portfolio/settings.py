4d287fcff9d3612dfae13f07739a098cd2e79b39
arpitverma9335 <asme.arpitverma19@gmail.com>
2021-04-14 20:07:26 +0530

    loader html

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = os.environ['secret_key_portfolio']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['127.0.0.1','varpit.herokuapp.com']
 
 
