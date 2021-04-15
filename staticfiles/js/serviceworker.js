2955f68910f87500e5d228a6dd1ce75180660d2f
arpitverma9335 <asme.arpitverma19@gmail.com>
2021-04-15 12:43:31 +0530

    sw

--- staticfiles/js/serviceworker.js
+++ staticfiles/js/serviceworker.js
@@ -4,18 +4,30 @@ self.addEventListener('install', function(event) {
 event.waitUntil(
   caches.open(staticCacheName).then(function(cache) {
   return cache.addAll([
+     '/static/img/arpit_profile.f8b5a837761f.jpg',
      '/static/img/banner/banner-2.62686fcfc741.jpg?5e0a4f7c70ab',
   ]);
   })
 );
 });
 
-self.addEventListener("fetch", event => {
+
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
+
+/*self.addEventListener("fetch", event => {
     if (event.request.url == "https://varpit.herokuapp.com/") {
         // or whatever your app's URL is
         event.respondWith(
             fetch(event.request).catch(err =>
-                self.cache.open(cache_name).then(cache => cache.match("'/static/img/banner/banner-2.62686fcfc741.jpg?5e0a4f7c70ab'"))
+                self.cache.open(cache_name).then(cache => cache.match(""))
             )
         );
     } else {
@@ -25,14 +37,5 @@ self.addEventListener("fetch", event => {
             )
         );
     }
-});
-
-/*self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
-
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
-   })
- );
 });*/
+
