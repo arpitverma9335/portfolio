f17951d5bbb45b73457d103208dba31f6846d95e
arpitverma9335 <asme.arpitverma19@gmail.com>
2021-11-26 12:09:44 +0530

    fixed serviceworker

--- staticfiles/js/serviceworker.js
+++ staticfiles/js/serviceworker.js
@@ -1,24 +1,44 @@
-var staticCacheName = 'djangopwa-v1';
+var staticCacheName = 'prefetch';
 
 self.addEventListener('install', function(event) {
 event.waitUntil(
   caches.open(staticCacheName).then(function(cache) {
   return cache.addAll([
      '/',
+     '/about',
+     '/contact',
+     '/blog',
   ]);
   })
 );
 });
 
 self.addEventListener("fetch", event => {
-    if (event.request.url === "https://www.simicart.com/") {
-        // or whatever your app's URL is
+    if (event.request.url === "https://varpit.herokuapp.com/") {
         event.respondWith(
             fetch(event.request).catch(err =>
                 self.cache.open(cache_name).then(cache => cache.match(""))
             )
         );
-    } else {
+    }else if (event.request.url === "https://varpit.herokuapp.com/about") {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("about"))
+            )
+        );
+    }else if (event.request.url === "https://varpit.herokuapp.com/contact") {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("contact"))
+            )
+        );
+    }else if (event.request.url === "https://varpit.herokuapp.com/blog") {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("blog"))
+            )
+        );
+    }else {
         event.respondWith(
             fetch(event.request).catch(err =>
                 caches.match(event.request).then(response => response)
@@ -27,12 +47,3 @@ self.addEventListener("fetch", event => {
     }
 });
 
-/*self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
-
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
-   })
- );
-});*/
